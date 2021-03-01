ae9402433c4c1befd006d60a5db1311d21c73229
murga <motiurga@gmail.com>
2021-02-28 22:34:30 -0800

    topic view now shows post's content

--- cforums/topic/views.py
+++ cforums/topic/views.py
@@ -4,6 +4,21 @@ from django.views import View
 from django.shortcuts import render
 from django.conf import settings
 
+from cforums.models import Post
+from cforums.models import Reply
+from cforums.models import Image
+
 class TopicView(View):
     def get(self, request, topic):
-        return render(request, "topic.html", {"topic": topic})
+        posts = []
+
+        for p in Post.objects.filter(topic=topic):
+            post = {
+                "title": p.title,
+                "creation_date": p.creation_date,
+                "id": p.pid,
+            }
+
+            posts.append(post)
+
+        return render(request, "topic.html", {"topic": topic, "posts": posts})

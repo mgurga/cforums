95378d6ef3afa0f4f88081e2759486ffeece6173
murga <motiurga@gmail.com>
2021-03-01 17:16:12 -0800

    if no posts exist return nothing

--- cforums/topic/views.py
+++ cforums/topic/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
 from django.conf import settings
+from django.db import OperationalError
 
 from cforums.models import Post
 from cforums.models import Reply
@@ -12,13 +13,16 @@ class TopicView(View):
     def get(self, request, topic):
         posts = []
 
-        for p in Post.objects.filter(topic=topic):
-            post = {
-                "title": p.title,
-                "creation_date": p.creation_date,
-                "id": p.pid,
-            }
+        try:
+            for p in Post.objects.filter(topic=topic):
+                post = {
+                    "title": p.title,
+                    "creation_date": p.creation_date,
+                    "id": p.pid,
+                }
 
-            posts.append(post)
+                posts.append(post)
+        except OperationalError:
+            pass
 
         return render(request, "topic.html", {"topic": topic, "posts": posts})

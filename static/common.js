377aae4a1e01a92407851f408eac05c81dd2e1be
murga <motiurga@gmail.com>
2021-03-02 18:36:30 -0800

    simplified css and moved header to new file

--- static/common.js
+++ static/common.js
@@ -1,13 +1,13 @@
-function loadhtml(element, url) {
+function loadhtml(element, url, onfinish) {
     httpreq = new XMLHttpRequest();
     httpreq.open('GET', url, true);
     httpreq.onload = function () {
         if (this.status >= 200 && this.status < 400) {
             element.innerHTML = this.response;
-            console.log(element);
+            onfinish();
         } else {
             console.log("error loading html");
         }
     }
-    httpreq.send()
+    httpreq.send();
 }
